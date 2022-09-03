02a58e5664e1c02aa2ba47cefc24a391a310fbf3
Pierre-J <pierre.primary@qq.com>
2022-09-04 02:52:58 +0800

    update alpine setup

--- alpine/setup-mysql.sh
+++ alpine/setup-mysql.sh
@@ -12,6 +12,38 @@ apk add mariadb
 # 初始化
 /etc/init.d/mariadb setup
 
+# 内存限制，小内存机器专用
+cat <<EOF | tee /etc/my.cnf.d/limit_mem.cnf
+[mysqld]
+performance_schema = off
+key_buffer_size = 32M
+query_cache_size = 1M
+query-cache-limit = 1M
+innodb_buffer_pool_size = 32M
+innodb_log_buffer_size = 8M
+max_connections = 50
+read_buffer_size = 64K
+read_rnd_buffer_size = 128K
+sort_buffer_size = 512K
+join_buffer_size = 128K
+# thread_stack = 196K
+# binlog_cache_size = 16K
+tmp_table_size = 8M
+EOF
+# SELECT  @@key_buffer_size  / (1024 * 1024) ;
+# SELECT  @@query_cache_size  / (1024 * 1024) ;
+# SELECT  @@query_cache_limit / (1024 * 1024) ;
+# SELECT  @@innodb_buffer_pool_size  / (1024 * 1024) ;
+# SELECT  @@innodb_log_buffer_size  / (1024 * 1024) ;
+# SELECT  @@max_connections ;
+# SELECT  @@read_buffer_size  / 1024 ;
+# SELECT  @@read_rnd_buffer_size  / 1024 ;
+# SELECT  @@sort_buffer_size  / 1024  ;
+# SELECT  @@join_buffer_size  / 1024  ;
+# SELECT  @@thread_stack  / 1024  ;
+# SELECT  @@binlog_cache_size  / 1024  ;
+# SELECT  @@tmp_table_size  / 1024  ;
+
 # 启动
 rc-update add mariadb default
 rc-service mariadb start
