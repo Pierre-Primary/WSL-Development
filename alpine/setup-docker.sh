dfe17d13c6666d65bb46af9beb78ef9b92a2073d
Pierre-J <pierre.primary@qq.com>
2022-09-06 07:27:25 +0800

    Update setup-docker.sh

--- alpine/setup-docker.sh
+++ alpine/setup-docker.sh
@@ -3,16 +3,21 @@ set -ex
 
 cd "$(dirname "$0")"
 
-# 安装 openrc
-./setup-openrc.sh
-
 # 安装服务
-apk add docker
+install_service() {
 
-# 备份配置
-mv /etc/conf.d/docker /etc/conf.d/docker.default
-# 修改配置
-cat <<EOF | tee /etc/conf.d/docker
+    type /usr/bin/dockerd >/dev/null && return
+
+    # 安装 openrc
+    ./setup-openrc.sh
+
+    # 安装服务
+    apk add docker
+
+    # 备份配置
+    mv /etc/conf.d/docker /etc/conf.d/docker.default
+    # 修改配置
+    cat <<EOF | tee /etc/conf.d/docker
 #DOCKER_LOGFILE="/var/log/docker.log"
 #DOCKER_OUTFILE="/var/log/docker-out.log"
 #DOCKER_ERRFILE="/var/log/docker-err.log"
@@ -21,10 +26,15 @@ cat <<EOF | tee /etc/conf.d/docker
 DOCKERD_BINARY="/usr/bin/dockerd"
 DOCKER_OPTS="-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375"
 EOF
-
-# 启动
-rc-update add docker default
-service docker start
+    # 启动
+    rc-update add docker default
+    ./setup-openrc.sh wait service docker start
+}
 
 # 安装客户端
-apk add docker-cli docker-cli-compose docker-cli-buildx
+install_cli() {
+    apk add docker-cli docker-cli-compose docker-cli-buildx
+}
+
+install_service
+install_cli
