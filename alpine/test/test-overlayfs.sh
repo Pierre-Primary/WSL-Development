c92732752ca4714fd30353d219c160773a81fec5
Pierre-J <pierre.primary@qq.com>
2022-09-09 07:11:42 +0800

    Update test-overlayfs.sh

--- alpine/test/test-overlayfs.sh
+++ alpine/test/test-overlayfs.sh
@@ -4,6 +4,7 @@ set -ex
 cd "$(dirname "$0")"
 
 SCRIPT_NAME=./"$(basename "$0")"
+INIT_CMD="/sbin/init"
 
 apk add openrc util-linux
 
@@ -24,7 +25,7 @@ if [ "$1" = "--enter" ]; then
 
     umount -l /parent && rm -rf /parent
 
-    exec /sbin/init
+    exec $INIT_CMD
 elif [ "$1" = "--init" ]; then
     rm -rf /overlay
 
@@ -36,19 +37,19 @@ elif [ "$1" = "--init" ]; then
 
     exec /usr/bin/env -i unshare -muipf --mount-proc --propagation=unchanged -- "$SCRIPT_NAME" --enter
 else
-    pid=$(ps -eo pid,args | awk '$2 ~ /^\/sbin\/init/ { print $1 }')
-    if [ -z "$pid" ]; then
+    INIT_PID=$(ps -eo pid,args | awk '$2 == "'"$INIT_CMD"'" { print $1; exit }')
+    if [ -z "$INIT_PID" ]; then
         /usr/bin/env -i unshare -muipf --mount-proc --propagation=unchanged -- "$SCRIPT_NAME" --init &
         set +x
-        times=0
-        while [ $times -lt 10 ]; do
-            pid=$(ps -eo pid,args | awk '$2 ~ /^\/sbin\/init/ { print $1 }')
-            [ -n "$pid" ] && break
-            times=$((times + 1))
+        WAIT_TIMES=0
+        while [ $WAIT_TIMES -lt 10 ]; do
+            INIT_PID=$(ps -eo pid,args | awk '$2 == "'"$INIT_CMD"'" { print $1; exit }')
+            [ -n "$INIT_PID" ] && break
+            WAIT_TIMES=$((WAIT_TIMES + 1))
             sleep 1
         done
         set -x
-        [ -z "$pid" ] && exit
+        [ -z "$INIT_PID" ] && exit
     fi
-    exec nsenter -a -t "$pid" --wdns="$(pwd)" -- /bin/sh
+    exec nsenter -a -t "$INIT_PID" -- /bin/sh -c "cd $(pwd); exec /bin/ash"
 fi
