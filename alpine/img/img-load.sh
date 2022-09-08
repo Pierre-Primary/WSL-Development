2a8941f465b18784015435b64d58f539153ebaab
Pierre-J <pierre.primary@qq.com>
2022-09-09 06:22:24 +0800

    Update img-load.sh

--- alpine/img/img-load.sh
+++ alpine/img/img-load.sh
@@ -3,23 +3,26 @@ set -ex
 
 cd "$(dirname "$0")"
 
+IMG_FILE=./output/alpine-3.16-docker.img
+
 if [ "$1" = "--enter" ]; then
-    PWD_PATH=$(pwd)
+    mount -t proc proc /rom/proc
 
-    mount -t proc proc /rom/root/proc
-
-    cd /rom/root
+    cd /rom
     mkdir -p parent
     pivot_root . parent
 
     mount --rbind /parent/dev /dev
     mount --rbind /parent/sys /sys
     mount --rbind /parent/mnt /mnt
+
+    mount -t tmpfs tmpfs /run
+    mount -t tmpfs tmpfs /tmp
+
     mount --rbind /parent/etc/hosts /etc/hosts
     mount --rbind /parent/etc/resolv.conf /etc/resolv.conf
-    umount -l /parent && rm -rf /parent
 
-    cd "$PWD_PATH"
+    umount -l /parent && rm -rf /parent
 
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
     apk update
@@ -27,12 +30,15 @@ if [ "$1" = "--enter" ]; then
 
     exec /sbin/init
 elif [ "$1" = "--init" ]; then
-    rm -rf /rom
-    mkdir -p /rom/rom /rom/root /rom/over /rom/upper /rom/work
-    mkdir -p /rom/over/etc
-    touch /rom/over/etc/hosts /rom/over/etc/resolv.conf
-    mount -t ext4 ./output/alpine-3.16-docker.img /rom/rom -o loop
-    mount -t overlay overlay /rom/root -o lowerdir=/rom/over:/rom/rom,upperdir=/rom/upper,workdir=/rom/work
+    rm -rf /overlay
+
+    mkdir -p /rom
+    mkdir -p /overlay/lower /overlay/upper /overlay/work
+    mkdir -p /overlay/over/etc && touch /overlay/over/etc/hosts /overlay/over/etc/resolv.conf
+
+    mount -o loop -t ext4 $IMG_FILE /overlay/lower
+    mount -t overlay overlay /rom -o lowerdir=/overlay/over:/overlay/lower,upperdir=/overlay/upper,workdir=/overlay/work
+
     exec /usr/bin/env -i unshare -muipf --mount-proc --propagation=unchanged -- "$0" --enter
 else
     pid=$(ps -eo pid,args | awk '$2 ~ /^\/sbin\/init/ { print $1 }')
@@ -47,6 +53,7 @@ else
             sleep 1
         done
         set -x
+        [ -z "$pid" ] && exit
     fi
     exec nsenter -a -t "$pid" --wdns="$(pwd)" -- /bin/sh
 fi
