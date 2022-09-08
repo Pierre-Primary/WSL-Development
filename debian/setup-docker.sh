eee7349de6f6172ede6eb82f8acf5b806f5b7024
Pierre-J <pierre.primary@qq.com>
2022-09-08 13:01:41 +0800

    Update setup-docker.sh

--- debian/setup-docker.sh
+++ debian/setup-docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+apt install curl gpg lsb-release
+
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" |
