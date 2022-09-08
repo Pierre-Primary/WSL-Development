9439a8b476345906ce41a153b791952649684a16
Pierre-J <pierre.primary@qq.com>
2022-09-08 16:10:27 +0800

    Update setup-docker.sh

--- debian/setup-docker.sh
+++ debian/setup-docker.sh
@@ -3,11 +3,16 @@ set -ex
 
 cd "$(dirname "$0")"
 
-./setup-systemd.sh
+if [ "$1" != "--isns" ]; then
+    ./setup-systemd.sh
+    /etc/wsl-init/enter "sleep 0.5; $0 --isns"
+    exit
+fi
 
 apt install -y curl gpg lsb-release
 
 mkdir -p /etc/apt/keyrings
+rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" |
     tee /etc/apt/sources.list.d/docker.list >/dev/null
@@ -17,4 +22,5 @@ apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sed -ri '/^ExecStart=\/usr\/bin\/dockerd/c ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 --containerd=/run/containerd/containerd.sock' /lib/systemd/system/docker.service
 
-/etc/wsl-init/enter "sleep 1; systemctl daemon-reload; systemctl restart docker.service"
+systemctl daemon-reload
+systemctl restart docker.service
