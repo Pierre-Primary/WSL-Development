5fffcc898ebdaf8e800b6b4b78268645a31afd82
Pierre-J <pierre.primary@qq.com>
2022-09-09 09:13:56 +0800

    Update test-veth.sh

--- alpine/test/test-veth.sh
+++ alpine/test/test-veth.sh
@@ -4,38 +4,47 @@ set -ex
 # https://blog.csdn.net/woshaguayi/article/details/114089866
 # https://blog.csdn.net/qq_40378034/article/details/123599477
 
-# apk add openrc
-apk add util-linux
-apk add iproute2
+ciptables() {
+    check=$(echo "$@" | sed -e 's/-A/-C/g')
+    if ! eval "$check" >/dev/null 2>&1; then
+        "$@"
+    fi
+}
 
-ip link add veth0.0 type veth peer name beth0.0
+cd "$(dirname "$0")"
 
-ip link add name br0 type bridge
-ip link set beth0.0 master br0
+SCRIPT_NAME=./"$(basename "$0")"
 
-ip addr add dev br0 192.168.10.1/24
+if [ "$1" = "--enter" ]; then
+    exec /bin/ash
+elif [ "$1" = "--init" ]; then
+    exec unshare -muipf --mount-proc --propagation=unchanged -- "$SCRIPT_NAME" --enter
+else
+    apk add util-linux iproute2 iptables
 
-ip link set beth0.0 up
-ip link set br0 up
+    set +e
+    ip netns pids n0 | xargs -I{} kill -SIGTERM {}
+    ip netns del n0
+    ip link del beth0.0
+    ip link del br0
+    set -e
 
-sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    ip link add veth0.0 type veth peer name beth0.0
+    ip link add name br0 type bridge
+    ip link set beth0.0 master br0
 
-ip netns add n0
-ip link set veth0.0 netns n0
-ip netns exec n0 ip addr add dev veth0.0 192.168.10.2/24
-ip netns exec n0 ip link set veth0.0 up
-ip netns exec n0 ip route add default via 192.168.10.1 dev veth0.0
+    ip addr add dev br0 192.168.10.1/24
 
-ip netns exec n0 /usr/bin/env -i unshare -mupf --mount-proc --propagation=unchanged -- sleep 3600 >/dev/null 2>&1 &
+    ip link set beth0.0 up
+    ip link set br0 up
 
-times=0
-while true; do
-    pid=$(ps -o pid,args | awk '$2 ~ /^sleep/ { print $1 }')
-    [ -n "$pid" ] && break
-    times=$((times + 1))
-    [ "$times" -ge 10 ] && exit 1
-    sleep 0.1
-done
+    sysctl -w net.ipv4.ip_forward=1
+    ciptables iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-exec /usr/bin/env -i nsenter -a -t "$pid"
+    ip netns add n0
+    ip link set veth0.0 netns n0
+    ip netns exec n0 ip addr add dev veth0.0 192.168.10.2/24
+    ip netns exec n0 ip link set veth0.0 up
+    ip netns exec n0 ip route add default via 192.168.10.1 dev veth0.0
+    ip netns exec n0 "$SCRIPT_NAME" --init
+fi
