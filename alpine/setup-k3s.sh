b89df6a6f516f4aab12d21c15bb7a9a867b3ae4d
Pierre-J <pierre.primary@qq.com>
2022-09-09 09:35:23 +0800

    Update setup-k3s.sh

--- alpine/setup-k3s.sh
+++ alpine/setup-k3s.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 set -ex
 
+# 安装并启动 openrc
+if [ "$1" != "--enter" ]; then
+    ./setup-openrc.sh
+    /etc/wsl-init/enter "$0 --enter"
+    exit
+fi
+
 cd "$(dirname "$0")"
 
 # 安装 docker
 ./setup-docker.sh
+
+apk add curl
 
 curl -sfL https://rancher-mirror.oss-cn-beijing.aliyuncs.com/k3s/k3s-install.sh |
     INSTALL_K3S_MIRROR=cn \
