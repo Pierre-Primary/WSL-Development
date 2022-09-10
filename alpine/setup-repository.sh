03471ddcef07175d20e418acfb4d9cdbebb0cdf8
Pierre-J <pierre.primary@qq.com>
2022-09-10 17:38:17 +0800

    Update setup-repository.sh

--- alpine/setup-repository.sh
+++ alpine/setup-repository.sh
@@ -1,6 +1,38 @@
-#!/usr/bin/env sh
-set -ex
+#!/usr/bin/env ash
+# shellcheck shell=bash
 
-# 换源
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+type apk >/dev/null 2>&1 || exit
+
+set -e
+cd "$(dirname "$0")"
+alias menu='../base/menu.sh'
+
+set +e
+menu -n \
+    -t "选择需要切换的仓库：" \
+    "中国科技大学" "清华大学" "南京大学" \
+    "阿里云" "腾讯云" "华为云"
+index=$?
+set -e
+
+echo $index
+
+case "$index" in
+1) domain=mirrors.ustc.edu.cn ;;
+2) domain=mirrors.tuna.tsinghua.edu.cn ;;
+3) domain=mirror.nju.edu.cn ;;
+4) domain=mirrors.aliyun.com ;;
+5) domain=mirrors.cloud.tencent.com ;;
+6) domain=repo.huaweicloud.com ;;
+*) exit ;;
+esac
+
+! [ -e /etc/apk/repositories.bak ] && cp /etc/apk/repositories /etc/apk/repositories.bak
+
+release=$(awk -F'.' ' { print $1"."$2 }' </etc/alpine-release)
+
+tee /etc/apk/repositories <<EOF
+http://${domain}/alpine/v${release}/main
+http://${domain}/alpine/v${release}/community
+EOF
 apk update
