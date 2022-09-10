921654cd5aeb6a24095e58c51ad1549ca0f4541d
Pierre-J <pierre.primary@qq.com>
2022-09-10 17:38:44 +0800

    Create menu.sh

---
+++ base/menu.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env sh
+set -e
+
+cd "$(dirname "$0")"
+
+alias input_control_key=./input-control-key.sh
+
+print_opts() {
+    _STX=$1 && shift
+    _SHOW_NUMBER=$1 && shift
+    _IDX=0
+    for _OPT; do
+        printf "\033[2K"
+        [ "$_SHOW_NUMBER" -eq 1 ] && printf "%d) " $((_IDX + 1))
+        [ "${_IDX}" -eq "${_STX}" ] && printf "\033[07m"
+        printf "%s" "$_OPT"
+        printf "\033[0m\n"
+        _IDX=$((_IDX + 1))
+    done
+}
+
+_STX=0
+_SHOW_NUMBER=0
+
+eval set -- "$(getopt -o t:i:n -l title:,index: -- "$@")"
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    -t | --title) _TITLE=$2 && shift ;;
+    -i | --index) _STX=$2 && shift ;;
+    -n) _SHOW_NUMBER=1 ;;
+    --) shift && break ;;
+    esac
+    shift
+done
+
+[ $# -eq 0 ] && exit
+
+[ -n "$_TITLE" ] && echo "$_TITLE"
+if input_control_key --check; then
+    trap 'printf "\033[?25h"' EXIT
+    printf "\033[?25l"
+    while true; do
+        _STX=$(((_STX + $#) % $#))
+        print_opts "$_STX" "$_SHOW_NUMBER" "$@"
+        case $(input_control_key) in
+        up) _STX=$((_STX - 1)) ;;
+        down) _STX=$((_STX + 1)) ;;
+        enter) break ;;
+        quit) exit 0 ;;
+        esac
+        printf '\033[%dA' $#
+    done
+else
+    print_opts "-1" "$_SHOW_NUMBER" "$@"
+    printf "请选择:"
+    read -r _STX
+fi
+exit $((_STX + 1))
+
+# echo "1xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# echo "2xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# echo "3xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# echo "4xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# echo "5xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# echo "6xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# printf "\033[5A"
+# printf "\033[K"
+# printf "\033[L"
+# printf "\033[2L"
+# printf "\033[J"
+# printf "\033[2J"
+# printf "\033[6n"
