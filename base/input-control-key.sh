d6d93925b2814d484730fa331a4cf17a83857a15
Pierre-J <pierre.primary@qq.com>
2022-09-11 20:14:04 +0800

    Update input-control-key.sh

--- base/input-control-key.sh
+++ base/input-control-key.sh
@@ -5,22 +5,25 @@ ESC=$(printf "\033")
 stty -echo
 while true; do
     stty -icanon min 1 time 0
-    _CHAR="$(dd bs=1 count=1 2>/dev/null)"
-    case $_CHAR in
-    "${ESC}")
-        stty -icanon min 0 time 1
-        _CHAR="$(dd bs=1 count=2 2>/dev/null)"
-        case $_CHAR in
-        "[A") echo "up" && break ;;
-        "[B") echo "down" && break ;;
-        "[C") echo "right" && break ;;
-        "[D") echo "left" && break ;;
-        "") echo "quit" && break ;;
+    _KEY="$(dd bs=1 count=1 2>/dev/null)"
+    case $_KEY in
+    "$ESC")
+        stty -icanon min 0 time 0
+        _KEY="$(dd 2>/dev/null)"
+        case $_KEY in
+        "[A") echo "Up" ;;
+        "[B") echo "Down" ;;
+        "[C") echo "Right" ;;
+        "[D") echo "Left" ;;
+        "") echo "Esc" ;;
+        *) continue ;;
         esac
         ;;
-    " ") echo "select" && break ;;
-    "") echo "enter" && break ;;
-    q | Q) echo "quit" && break ;;
+    " ") echo "Space" ;;
+    "") echo "Enter" ;;
+    q | Q) echo "Esc" ;;
+    *) continue ;;
     esac
+    [ "$1" != "--loop" ] && break
 done
 stty "$OLD_STTY"
