0c2e58ee29a382f277e405a5780a4a1e44903fc2
Pierre-J <pierre.primary@qq.com>
2022-09-12 21:02:21 +0800

    Update setup-docker.sh

--- debian/setup-docker.sh
+++ debian/setup-docker.sh
@@ -8,8 +8,6 @@ $SUDO apt update
 # 卸载旧版
 $SUDO apt remove -y docker docker-engine docker.io containerd runc 2>/dev/null
 
-set -e
-
 # 安装工具
 $SUDO apt install -y \
     ca-certificates \
@@ -17,17 +15,21 @@ $SUDO apt install -y \
     gnupg \
     lsb-release
 
+########################################################################################################
+
 # 配置 Docker 软件源密钥
 $SUDO mkdir -p /etc/apt/keyrings
 $SUDO rm -f /etc/apt/keyrings/docker.gpg
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg |
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg |
     $SUDO gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # 配置 Docker 软件源
 $SUDO tee /etc/apt/sources.list.d/docker.list >/dev/null <<EOF
-deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable
+deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable
 EOF
 $SUDO apt update
+
+########################################################################################################
 
 # 安装 Docker
 $SUDO apt install -y \
@@ -41,9 +43,18 @@ $SUDO sed -ri '/^ExecStart=/c ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0
 $SUDO systemctl daemon-reload
 
 # 重启 docker 服务
-$SUDO systemctl restart docker.service
+$SUDO systemctl restart docker
 
-# 卸载
-# sudo apt remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+########################################################################################################
+# 生成卸载脚本
 
-# sudo mount -t vboxsf share /mnt/g
+$SUDO tee /usr/local/bin/docker-uninstall <<EOF >/dev/null
+#!/usr/bin/env bash
+set -x
+type sudo >/dev/null 2>&1 && SUDO="sudo"
+
+\$SUDO apt autoremove -y --purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+\$SUDO rm -f /usr/local/bin/docker-uninstall
+EOF
+$SUDO chmod u+x /usr/local/bin/docker-uninstall
